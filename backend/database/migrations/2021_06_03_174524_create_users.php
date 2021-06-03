6ab932bee443bef619a98f71e5b2cdf9a074786f
Kaio Borges <kaa.borges10@hotmail.com>
2021-06-03 14:51:43 -0300

    fix: small fixes in migrations

--- backend/database/migrations/2021_06_03_174524_create_users.php
+++ backend/database/migrations/2021_06_03_174524_create_users.php
@@ -18,13 +18,12 @@ class CreateUsers extends Migration
             $table->string('name', 100);
             $table->string('email', 100);
             $table->string('telefone', 50);
-            $table->time('password', 200);
+            $table->string('password', 200);
             $table->date('birthdate');
             $table->string('address', 200);
             $table->string('addon', 200);
             $table->string('neighborhood', 200);
             $table->string('cep', 200);
-            $table->string('neighborhood', 200);
             $table->string('token', 200)->nullable();
             $table->timestamps();
         });
--- backend/database/migrations/2021_06_03_174533_create_pastries.php
+++ backend/database/migrations/2021_06_03_174533_create_pastries.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePastry extends Migration
+class CreatePastries extends Migration
 {
     /**
      * Run the migrations.
