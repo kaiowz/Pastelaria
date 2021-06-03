ad882577816ae6410470aa48815fdd13b1f2c771
Kaio Borges <kaa.borges10@hotmail.com>
2021-06-03 14:28:05 -0300

    feat: api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PastryController;
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::prefix('auth')->group(function(){
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/register', [AuthController::class, 'create']);
+});
+
+Route::prefix('user')->group(function(){
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::get('/me', [UserController::class, 'me']);
+    Route::get('/{id}', [UserController::class, 'index']);
+    Route::get('/', [UserController::class, 'index']);
+    Route::delete('/{id}', [UserController::class, 'delete']);
+});
+
+Route::prefix('pastry')->group(function(){
+    Route::post('/', [PastryController::class, 'create']);
+    Route::put('/{id}', [PastryController::class, 'update']);
+    Route::get('/{id}', [PastryController::class, 'index']);
+    Route::get('/', [PastryController::class, 'index']);
+    Route::delete('/{id}', [PastryController::class, 'delete']);
 });
